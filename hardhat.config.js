f85e19c0e7550a13c2581ace65161bfe479cde2e
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2025-07-10 01:38:23 -0700

    Create erc20 token contract, setup hardhat configuration for flow network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const privateKey = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    flowtestnet: {
+      url: `https://flow-testnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [privateKey]
+    }
+  }
 };
