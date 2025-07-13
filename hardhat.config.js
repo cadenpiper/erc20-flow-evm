5d809c59249d2d0d56616ee3aabb8252614bc40e
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2025-07-12 19:38:37 -0700

    Deploy Nocenix to flow testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition");
 require("dotenv").config();
 
 const privateKey = process.env.PRIVATE_KEY;
@@ -7,7 +8,7 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     hardhat: {},
-    flowtestnet: {
+    flowTestnet: {
       url: `https://flow-testnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [privateKey]
     }
