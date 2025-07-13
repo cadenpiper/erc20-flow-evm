3aaf6fb1f952d89797f7dfa2cd7540852c6f03b1
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2025-07-12 17:50:30 -0700

    Correct contract & clean up files

--- contracts/Nocenix.sol
+++ contracts/Nocenix.sol
@@ -2,18 +2,22 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20, ERC20Burnable, Ownable {
+contract Token is ERC20, Ownable {
+
     constructor(address initialOwner)
         ERC20("Nocenix", "NCX")
-        Ownable(initialOwner)
+        Ownable(msg.sender)
     {
         _mint(initialOwner, 1000000000 * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function mint(address _to, uint256 _amount) external  onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external {
+        _burn(_from, _amount);
     }
 }
